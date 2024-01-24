5d98e355aeee2c4347107d80291150bba7dfe170
Will <williampetersmeyer@gmail.com>
2024-01-23 23:46:06 -0800

    CMD line parameters fixed

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ Copyright (c) 2023 Wilhelm, Donkey
 */
 
 use std::env;
+use std::process::exit;
 use terminal_link::Link;
 
 mod checks;
@@ -19,16 +20,26 @@ mod checks;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 0 {
-        return;
+    if args.len() <= 1 {
+        exit(0);
     }
 
-    match &args[0] {
+    match args[1].as_str() {
 
-        _help => {
+        "--test" => {
+            println!("Testing check functions...");
+
+            println!("Done");
+        },
+
+        "--help" => {
             println!("Please see Merlin Engine documentation for an explanation of command-line parameters.");
             println!("Click {} to visit the wiki.", Link::new("here", "https://github.com/Merlin-Lovers-Anonymous/merlin-engine/wiki"))
         },
+
+        _other => {
+            println!("{} is not a recognized command-line parameter.", args[1]);
+        }
 
     }
 }
